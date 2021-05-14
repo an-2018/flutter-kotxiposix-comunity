b4f5bfa432e65b182ea988cc1003cd224b6fe850
anilson <anilsonmonteiro@gmail.com>
2021-05-14 22:19:09 -0100

    update card layout

--- lib/pages/LiveChannels/live_channels.dart
+++ lib/pages/LiveChannels/live_channels.dart
@@ -11,7 +11,7 @@ class LiveChannels extends StatefulWidget {
 }
 
 class _LiveChannelsState extends State<LiveChannels> {
-  Future<List<Album>> futureAlbum;
+  Future<Album> futureAlbum;
 
   @override
   void initState() {
@@ -36,22 +36,21 @@ class _LiveChannelsState extends State<LiveChannels> {
                 color: Colors.grey, borderRadius: BorderRadius.circular(10)),
           ),
           CardWidget(
-            imageUri: "images/hero-image.png",
+            imageUri:
+                "https://cdn.vox-cdn.com/thumbor/snH4LjjNq_uyiyxi3ISEqi6WcDg=/1400x1400/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/22182760/patch_10_25_banner.jpg",
             title: "2020 World Champing Warzone",
           ),
-          FutureBuilder<List<Album>>(
+          FutureBuilder<Album>(
               future: futureAlbum,
               builder: (context, snapshot) {
                 if (snapshot.hasData) {
-                  return ListView(
-                    children: [
-                      for (var album in snapshot.data){ 
-                      Text("${album.title}");
-                      print();
-                      }
-                    ],
+                  return CardWidget(
+                    title: snapshot.data.title,
+                    imageUri:
+                        "https://miro.medium.com/max/1400/1*8U9my6iR2E1asU7qXsA08Q.jpeg",
+                    text1: snapshot.data.id.toString(),
+                    text2: snapshot.data.id.toString(),
                   );
-                  //Text(snapshot.data.title);
                 } else if (snapshot.hasError) {
                   print("${futureAlbum} error");
                   return Text("${snapshot.error}");
@@ -59,6 +58,24 @@ class _LiveChannelsState extends State<LiveChannels> {
 
                 return CircularProgressIndicator();
               }),
+          Container(
+            height: 300,
+            child: GridView.count(
+              crossAxisCount: 2,
+              children: [
+                CardWidget(
+                  imageUri:
+                      "https://cdn.vox-cdn.com/thumbor/snH4LjjNq_uyiyxi3ISEqi6WcDg=/1400x1400/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/22182760/patch_10_25_banner.jpg",
+                  title: "2020 World Champing Warzone",
+                ),
+                CardWidget(
+                  imageUri:
+                      "https://cdn.vox-cdn.com/thumbor/snH4LjjNq_uyiyxi3ISEqi6WcDg=/1400x1400/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/22182760/patch_10_25_banner.jpg",
+                  title: "2020 World Champing Warzone",
+                ),
+              ],
+            ),
+          )
         ],
       ),
     );
@@ -82,9 +99,8 @@ class CardWidget extends StatelessWidget {
               height: 200.0,
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(10),
-                color: Colors.amber,
                 image: DecorationImage(
-                  image: AssetImage(imageUri),
+                  image: NetworkImage(imageUri),
                   fit: BoxFit.cover,
                 ),
               ),
@@ -97,7 +113,7 @@ class CardWidget extends StatelessWidget {
                 alignment: Alignment.centerLeft,
                 width: double.maxFinite,
                 height: Theme.of(context).textTheme.bodyText1.fontSize,
-                child: Icon(Icons.ac_unit_sharp),
+                child: Text(text1 ?? "text1"),
               ),
             ),
             ListTile(
@@ -105,7 +121,7 @@ class CardWidget extends StatelessWidget {
                 alignment: Alignment.centerLeft,
                 width: double.maxFinite,
                 height: Theme.of(context).textTheme.bodyText1.fontSize,
-                child: Icon(Icons.ac_unit_sharp),
+                child: Text(text2 ?? "text2"),
               ),
             ),
           ],
@@ -113,4 +129,15 @@ class CardWidget extends StatelessWidget {
       ),
     );
   }
+}
+
+List<Widget> getCategoryList() {
+  return [
+    Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Container(
+          color: Colors.amber,
+          child: Center(child: Text("name")),
+        ))
+  ];
 }
